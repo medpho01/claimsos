bc1c1dcf6eec6e0e325e1cf0f8cd44429f92a828
ankitmedpho <ankit.meena@medpho.com>
2025-12-30 12:26:14 +0530

    Token generator

---
+++ Backend/src/Utils/tokens.util.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { v4 as uuidv4 } from 'uuid';
+
+import type { StringValue } from "ms";
+
+
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+const ACCESS_TOKEN_EXPIRY: StringValue = (process.env.ACCESS_TOKEN_EXPIRY || '1h') as StringValue;
+const REFRESH_TOKEN_EXPIRY_DAYS: number = parseInt(process.env.REFRESH_TOKEN_EXPIRY_DAYS || '7', 10);
+
+export const generateAccessToken = (userId: string) => {
+  if (!ACCESS_TOKEN_SECRET) {
+    throw new Error('ACCESS_TOKEN_SECRET is not defined');
+  }
+  return jwt.sign({ id: userId }, ACCESS_TOKEN_SECRET, {
+    expiresIn: ACCESS_TOKEN_EXPIRY,
+  });
+};
+
+export const generateRefreshToken = () => {
+  const token = uuidv4(); 
+  const expiresAt = new Date();
+  expiresAt.setDate(expiresAt.getDate() + REFRESH_TOKEN_EXPIRY_DAYS);
+
+  return { token, expiresAt };
+};
