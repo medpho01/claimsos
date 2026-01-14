8625169ccf960381f34eb5c09d61f63ba3a6015e
moneygoyal-del <money.goyal@medpho.com>
2026-01-14 14:06:50 +0530

    fix(schema)

--- Backend/src/schema/schema.sql
+++ Backend/src/schema/schema.sql
@@ -15,7 +15,8 @@ CREATE TABLE IF NOT EXISTS hospital.users (
     last_login TIMESTAMP,
     folder_id VARCHAR(255) UNIQUE, -- Google Drive folder ID for the hospital
     hospital_group_id VARCHAR(255) DEFAULT NULL,
-    sheet_link VARCHAR(255) DEFAULT NULL,
+    sheet_id VARCHAR(255) DEFAULT NULL, -- Google Sheets spreadsheet ID
+    sheet_name VARCHAR(255) DEFAULT NULL, -- Google Sheets sheet name
     created_at TIMESTAMP DEFAULT NOW(),
     updated_at TIMESTAMP DEFAULT NOW()
 );
@@ -34,7 +35,6 @@ CREATE TABLE IF NOT EXISTS hospital.patients(
     admitted_at TIMESTAMP DEFAULT NOW(),
     discharged_at TIMESTAMP DEFAULT NULL,
     folder_id VARCHAR(255), -- Google Drive folder ID for patient's images
-    admission_type VARCHAR(30), -- conservative,surgical
     pmjay_case_number VARCHAR(100), -- PMJAY Case Number 
     scheme VARCHAR(100), -- Scheme 
     treatment_procedure TEXT, -- Treatment/Procedure details
